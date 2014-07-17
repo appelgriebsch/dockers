d97c728d174bcbc69b29a708d637c7745750aed8
root <root@dockers.appelgriebsch.org>
2014-07-17 12:24:40 -0400

    open up database access from outside

--- docker/postgresql/initDB.sh
+++ docker/postgresql/initDB.sh
@@ -3,6 +3,7 @@
 echo Initialize the database...
 /usr/pgsql-9.3/bin/initdb -D /data/postgresql -E UTF-8
 sed -i "s/^\(#listen_addresses.*\)$/listen_addresses = '*'\n\1/" /data/postgresql/postgresql.conf
+echo "host all  all    0.0.0.0/0  md5" >> /data/postgresql/pg_hba.conf
 
 echo Start the database server...
 /usr/pgsql-9.3/bin/postgres -D /data/postgresql &
