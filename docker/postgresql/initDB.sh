3fd65a341755beff9d1bd634571a2b246d21835c
root <root@dockers.appelgriebsch.org>
2014-07-17 05:05:30 -0400

    initial support for postgresql 9.3 on centos7

---
+++ docker/postgresql/initDB.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Initialize the database...
+/usr/pgsql-9.3/bin/initdb -D /data/postgresql -E UTF-8
+
+echo Start the database server...
+/usr/pgsql-9.3/bin/postgres -D /data/postgresql &
+PGID=$!
+
+echo Creating database user...
+ 
+DBUSR=dbUser
+DBPWD=md5$(echo "!dbUseR!" | md5sum)
+sleep 2 
+/usr/pgsql-9.3/bin/psql --command="CREATE ROLE \"$DBUSR\" PASSWORD '\"$DBPWD\"' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+
+echo Stopping the database server...
+sleep 2 
+kill $PGID
