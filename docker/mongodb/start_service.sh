826ddf915661286ef812a3a7a913d46be0aacda1
andi_appelgriebsch_com <andi_appelgriebsch_com@centos-1.c.wide-jigsaw-400.internal>
2014-08-11 14:46:35 +0000

    updated monogdb configs and docker file

--- docker/mongodb/start_service.sh
+++ docker/mongodb/start_service.sh
@@ -16,7 +16,7 @@ fi
 echo Checking network binding... $MONGO_BIND
 if [ "$MONGO_BIND" == "127.0.0.1" ]; then
   echo Disable public network binding, connection has to take place via unix socket...
-  echo "unixSocketPrefix=$MONGO_DBDIR/$HOSTNAME" >> $MONGO_DBDIR/$HOSTNAME/mongod.conf
+  echo "unixSocketPrefix = $MONGO_DBDIR/$HOSTNAME" >> $MONGO_DBDIR/$HOSTNAME/mongod.conf
 else
   echo Enabling network binding on IP $MONGO_BIND.
   sed -i "s/^\(bind_ip.*\)$/# \1\nbind_ip = $MONGO_BIND/" $MONGO_DBDIR/$HOSTNAME/mongod.conf
@@ -25,13 +25,13 @@ fi
 echo Checking network port... $MONGO_PORT
 if ! [ -z "$MONGO_PORT" ]; then
   echo Enabling network port $MONGO_PORT.
-  sed -i "s/^\(#port.*\)$/# \1\nport = $MONGO_PORT/" $MONGO_DBDIR/$HOSTNAME/mongod.conf
+  sed -i "s/^\(#port.*\)$/\1\nport = $MONGO_PORT/" $MONGO_DBDIR/$HOSTNAME/mongod.conf
 fi
 
 echo Checking verbose logging information... $MONGO_VERBOSE
 if [ "$MONGO_VERBOSE" == "1" ]; then
   echo Enabling verbose log level...
-
+  sed -i "s/^\(#verbose.*\)$/\1\nverbose = true/" $MONGO_DBDIR/$HOSTNAME/mongod.conf
 fi
 
 /usr/bin/mongod -f $MONGO_DBDIR/$HOSTNAME/mongod.conf
