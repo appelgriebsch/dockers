11f61a8e9ec69e4b29a0f4da23f8415c2717e3c1
Andreas Gerlach <andi@appelgriebsch.com>
2014-08-18 15:57:46 +0200

    bugfix init-script and installation dependencies   # Please enter the commit message for your changes. Lines starting

--- docker/postgresql/start_instance.sh
+++ docker/postgresql/start_instance.sh
@@ -36,17 +36,17 @@ function maskPath() {
 }
 
 function waitForConnection() {
-  
+
   local host=$1
   local port=$2
- 
-  while ! echo exit | nc $host $port; do sleep 10; done 
+
+  while ! echo exit | nc $host $port; do sleep 10; done
 }
 
 function resolveIPAddress() {
 
   local host=$1
-  
+
   if [ $host == $HOSTNAME ]; then
     local myIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
     echo $myIP
@@ -57,14 +57,14 @@ function resolveIPAddress() {
 }
 
 function createInstanceDirectories() {
-  
+
   if [ -n "$PGSQL_DBDIR" ]; then
     echo Creating database path $PGSQL_DBDIR/$HOSTNAME.
     mkdir -p $PGSQL_DBDIR/$HOSTNAME
   fi
 
   echo Setting database path to $PGSQL_DBDIR.
-  chown -R postgres:postgres $PGSQL_DBDIR && \ 
+  chown -R postgres:postgres $PGSQL_DBDIR && \
   chmod -R 755 $PGSQL_DBDIR/$HOSTNAME
   /usr/pgsql-9.3/bin/initdb -D $PGSQL_DBDIR/$HOSTNAME -E UTF-8
 
@@ -76,20 +76,20 @@ function setupInstanceNetworking() {
   echo Checking network binding... $PGSQL_BIND
   if [ "$PGSQL_BIND" == "127.0.0.1" ]; then
     echo Disable public network binding, connection has to take place via unix socket...
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "s/^\(#unix_socket_directories .*\)$" "\1\nunix_socket_directories = '$PGSQL_DBDIR/$HOSTNAME'"
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "s/^\(#unix_socket_group .*\)$" "\1\nunix_socket_group = 'postgres'"
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "s/^\(#unix_socket_permissions .*\)$" "\1\nunix_socket_permissions = '0755'"
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#unix_socket_directories.*\)$" "\1\nunix_socket_directories = '$PGSQL_DBDIR/$HOSTNAME'"
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#unix_socket_group.*\)$" "\1\nunix_socket_group = 'postgres'"
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#unix_socket_permissions.*\)$" "\1\nunix_socket_permissions = '0755'"
     addToFile $PGSQL_DBDIR/$HOSTNAME/pg_hba.conf "local all all md5"
   else
     echo Enabling network binding on IP $PGSQL_BIND.
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "s/^\(#listen_addresses.*\)$/listen_addresses = '$PGSQL_BIND'\n\1/"
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#listen_addresses.*\)$/listen_addresses = '$PGSQL_BIND'\n\1/"
     addToFile $PGSQL_DBDIR/$HOSTNAME/pg_hba.conf "host all  all    $PGSQL_BIND  md5"
   fi
 
   echo Checking network port... $PGSQL_PORT
   if [ -n "$PGSQL_PORT" ]; then
     echo Enabling network port $PGSQL_PORT.
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#port .*\)$" "\1\nport = $PGSQL_PORT"
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "s/^\(#port.*\)$" "\1\nport = $PGSQL_PORT"
   fi
 
   return 0
@@ -108,7 +108,7 @@ function configureInstance() {
   fi
   echo Checking database log level... $PGSQL_LOGLVL
   if [ -n "$PGSQL_LOGLVL" ]; then
-    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#log_min_messages .*\)$" "\1\nlog_min_messages = $PGSQL_LOGLVL" 
+    replaceInFile $PGSQL_DBDIR/$HOSTNAME/postgresql.conf "^\(#log_min_messages .*\)$" "\1\nlog_min_messages = $PGSQL_LOGLVL"
   fi
 }
 
@@ -122,7 +122,7 @@ function startInstance() {
     set -m
     $INSTANCE_CMD &
     # wait for connection to MASTER server and LOCALHOST
-    waitForConnection 127.0.0.1 $PGSQL_PORT 
+    waitForConnection 127.0.0.1 $PGSQL_PORT
     echo Executing local configuration script on server 127.0.0.1:$PGSQL_PORT
     initStatus=$(/usr/pgsql-9.3/bin/psql --file=/tmp/initInstance.sql)
     echo Initialize status: $initStatus
