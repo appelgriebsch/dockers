420814e4fcfb9454248f3495ee76c56c4b6db583
Andreas Gerlach <andi@appelgriebsch.com>
2015-11-20 11:26:53 +0100

    small bugfix for mongodb container

--- mongodb/start_instance.sh
+++ mongodb/start_instance.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export MONGO_DBDIR=/data/mongodb
 
 function replaceInFile() {
 
@@ -36,17 +37,17 @@ function maskPath() {
 }
 
 function waitForConnection() {
-  
+
   local host=$1
   local port=$2
- 
-  while ! echo exit | nc $host $port; do sleep 10; done 
+
+  while ! echo exit | nc $host $port; do sleep 10; done
 }
 
 function resolveIPAddress() {
 
   local host=$1
-  
+
   if [ $host == $HOSTNAME ]; then
     local myIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
     echo $myIP
@@ -81,17 +82,17 @@ function setupInstanceNetworking() {
   echo Checking network binding... $MONGO_BIND
   if [ "$MONGO_BIND" == "127.0.0.1" ]; then
     echo Disable public network binding, connection has to take place via unix socket...
-    addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "net.unixDomainSocket.enabled: true" 
-    addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "net.unixDomainSocket.pathPrefix: $MONGO_DBDIR/$HOSTNAME" 
+    addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "net.unixDomainSocket.enabled: true"
+    addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "net.unixDomainSocket.pathPrefix: $MONGO_DBDIR/$HOSTNAME"
   else
     echo Enabling network binding on IP $MONGO_BIND.
-    replaceInFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "^\(net.bindIp.*\)$" "net.bindIp: $MONGO_BIND" 
+    replaceInFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "^\(net.bindIp.*\)$" "net.bindIp: $MONGO_BIND"
   fi
 
   echo Checking network port... $MONGO_PORT
   if [ -n "$MONGO_PORT" ]; then
     echo Enabling network port $MONGO_PORT.
-    replaceInFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "^\(net.port.*\)$" "net.port: $MONGO_PORT" 
+    replaceInFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "^\(net.port.*\)$" "net.port: $MONGO_PORT"
   fi
 
   return 0
@@ -100,7 +101,7 @@ function setupInstanceNetworking() {
 function configureReplicaSet() {
 
   echo Joining ReplicaSet... $MONGO_REPLSET
-  addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "replication.replSetName: $MONGO_REPLSET" 
+  addToFile $MONGO_DBDIR/$HOSTNAME/mongod.conf "replication.replSetName: $MONGO_REPLSET"
 
   MONGO_MASTER_IP='127.0.0.1'
   MONGO_MASTER_PORT=$MONGO_PORT
@@ -109,14 +110,14 @@ function configureReplicaSet() {
     MONGO_MASTER_IP=$(replaceInString $MONGO_MASTER "^\([0-9A-Za-z._\-]*\):\([0-9]*\)$" "\1")
     MONGO_MASTER_PORT=$(replaceInString $MONGO_MASTER "^\([0-9A-Za-z._\-]*\):\([0-9]*\)$" "\2")
   fi
-  
+
   local myIP=$(resolveIPAddress $HOSTNAME)
 
   addToFile /tmp/initInstance.js "// initialize and configure replica sets"
   addToFile /tmp/initInstance.js "var replStatus = rs.status();"
   addToFile /tmp/initInstance.js "var result = {};"
   addToFile /tmp/initInstance.js "if (replStatus.ok == 0) { result = rs.initiate({ _id: '$MONGO_REPLSET', version: 1, members: [ { _id: 0, host: '$myIP:$MONGO_PORT' } ]}); }"
-  
+
   if ! [ "$MONGO_MASTER_IP" == "127.0.0.1" ]; then
     if [ "$MONGO_TYPE" == "normal" ]; then
       addToFile /tmp/initInstance.js "result = rs.add('$myIP:$MONGO_PORT');"
@@ -127,7 +128,7 @@ function configureReplicaSet() {
   fi
 
   addToFile /tmp/initInstance.js "printjson(result);"
-  
+
   return 0
 }
 
@@ -148,7 +149,7 @@ function configureInstance() {
     echo Enabling verbose log level... $MONGO_LOGLVL
     sed -i "s/^\(systemLog.verbosity.*\)$/systemLog.verbosity: $MONGO_LOGLVL/" $MONGO_DBDIR/$HOSTNAME/mongod.conf
   fi
-  
+
   echo Checking instance type... $MONGO_TYPE
   case $MONGO_TYPE in
     normal)
@@ -168,15 +169,15 @@ function configureInstance() {
     *)
       echo Unknown instance type: $MONGO_TYPE
       exit 1
-  esac 
+  esac
 
   return 0
 }
 
 function startInstance() {
- 
+
   INSTANCE_CMD="/usr/bin/mongod -f $MONGO_DBDIR/$HOSTNAME/mongod.conf"
-  
+
   if [ -f /tmp/initInstance.js ]; then
     echo Local configuration script found. Starting instance in background...
     # Turn on monitor mode to allow switching between background and foreground processes
