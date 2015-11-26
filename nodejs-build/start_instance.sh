be3c63438efb2ee648f99b0ab92228489b2a8cab
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2015-11-26 20:52:04 +0100

    copy new build script from dev branch to stable

--- nodejs-build/start_instance.sh
+++ nodejs-build/start_instance.sh
@@ -1,53 +1,70 @@
 #!/bin/bash
 
-echo Cleanup build directory...
-rm -rf /data/build/.* /data/build/* 1> /dev/null 2>&1
+BUILD_ID=$(uuidgen)
 
-echo Copy source files to build directory...
-cp -R /data/src/. /data/build
+echo Creating temporary build directory /tmp/$BUILD_ID
+mkdir -p /tmp/$BUILD_ID
 
-echo Cleanup obsolete files from build directory...
-rm -rf /data/build/node_modules /data/build/.git
+if [ -n "$GIT_REPO" ]; then
+  echo Cloning GIT repo from $GIT_REPO
 
-if [ -f /data/build/dependencies.lst ]; then
+  if [ -n "$GIT_USER" ]; then
+    git clone "https://$GIT_USER:$GIT_TOKEN@$GIT_REPO" /tmp/$BUILD_ID
+  else
+    git clone "https://$GIT_REPO" /tmp/$BUILD_ID
+  fi
+else
+  echo Copy source files to build directory...
+  cp -R /data/build/. /tmp/$BUILD_ID
+fi
+
+echo Cleanup obsolete files from temporary build directory...
+rm -rf /tmp/$BUILD_ID/node_modules /tmp/$BUILD_ID/.git
+
+if [ -f /tmp/$BUILD_ID/dependencies.lst ]; then
   echo Installing native dependencies...
   apk update
-  MODULES=$(cat /data/build/dependencies.lst | tr '\n' ' ')
+  MODULES=$(cat /tmp/$BUILD_ID/dependencies.lst | tr '\n' ' ')
   apk add $MODULES
   rm -rf /var/cache/apk/*
 fi
 
-if [ -f /data/build/bower.json ]; then
+cd /tmp/$BUILD_ID
+
+if [ -f bower.json ]; then
   echo Installing Bower...
   npm i -g bower
-
+  echo Running bower install in temporary build directory...
   bower install --allow-root
 fi
 
-if [ -f /data/src/webpack.config.js ]; then
+if [ -f webpack.config.js ]; then
   echo Install webpack...
   npm i -g webpack
 fi
 
-if [ -f /data/build/package.json ]; then
-  echo Running npm install in build directory...
+if [ -f package.json ]; then
+  echo Running npm install in temporary build directory...
   npm install --unsafe-perm && npm prune && npm cache clean
 fi
 
-if ls /data/src/Gruntfile* 1> /dev/null 2>&1; then
+if ls Gruntfile* 1> /dev/null 2>&1; then
   echo Installing Grunt...
   npm i -g grunt-cli
-
-  echo Starting Grunt build process in build directory...
+  echo Starting Grunt build process in temporary build directory...$@
   grunt $@
 fi
 
-if ls dist/Gulpfile* 1> /dev/null 2>&1; then
+if ls Gulpfile* 1> /dev/null 2>&1; then
   echo Installing Gulp...
   npm i -g gulp
-
-  echo Starting Gulp build process in build directory...
+  echo Starting Gulp build process in temporary build directory...$@
   gulp $@
+fi
+
+if [ -n "$PROJ_NAME" ]; then
+  echo Generating Release $PROJ_NAME-$PROJ_VER-Release.tar.gz
+  tar -czf /data/build/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
 fi
 
 echo Finished.
