8b59974a7af8e3bc70b5f6de047e624dcd497d47
Andreas Gerlach <andreas.gerlach@wincor-nixdorf.com>
2016-04-06 16:02:25 +0200

    add missing files from merge

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ dockers
 =======
 
  Library of highly configurable docker containers
-
- Specific configuration can be found in stable and dev branches!
---
+++ nodejs-runtime/start_instance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function createInstanceDirectories() {
+  return 0
+}
+
+function setupInstanceNetworking() {
+
+  return 0
+}
+
+function configureInstance() {
+
+  return 0
+}
+
+function startInstance() {
+
+  npm start -- $@
+  return 0
+}
+
+createInstanceDirectories && setupInstanceNetworking && \
+ configureInstance && startInstance
