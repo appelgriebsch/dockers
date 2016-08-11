c22e606bf3dd3f82521658e122c32c82883acbcf
Andreas Gerlach <agerlach@INNO-DEV-SFC1.innolab.local>
2016-08-11 11:27:02 +0200

    updated build scripts

--- golang-build/start_instance.sh
+++ golang-build/start_instance.sh
@@ -15,16 +15,16 @@ if [ -n "$GIT_REPO" ]; then
   fi
 else
   echo Copy source files to build directory...
-  cp -R /data/build/. /tmp/$BUILD_ID
+  cp -R /data/src/. /tmp/$BUILD_ID
 fi
 
 echo Cleanup obsolete files from temporary build directory...
-rm -rf /tmp/$BUILD_ID/node_modules /tmp/$BUILD_ID/.git
+rm -rf /tmp/$BUILD_ID/.git
 
-if [ -f /tmp/$BUILD_ID/dependencies.lst ]; then
-  echo Installing native dependencies...
+if [ -f /tmp/$BUILD_ID/devDependencies.lst ]; then
+  echo Installing native dev dependencies...
   apk update
-  MODULES=$(cat /tmp/$BUILD_ID/dependencies.lst | tr '\n' ' ')
+  MODULES=$(cat /tmp/$BUILD_ID/devDependencies.lst | tr '\n' ' ')
   apk add $MODULES
   rm -rf /var/cache/apk/*
 fi
@@ -32,11 +32,11 @@ fi
 cd /tmp/$BUILD_ID
 export GOPATH=$(pwd)
 go get -d
-go build -o $PROJ_NAME
+go build -o $PROJ_NAME $BUILD_ARGS
 
 if [ -n "$PROJ_NAME" ]; then
   echo Generating Release $PROJ_NAME-$PROJ_VER-Release.tar.gz
-  tar -czf /data/build/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
+  tar -czf /data/src/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
 fi
 
 echo Finished.
