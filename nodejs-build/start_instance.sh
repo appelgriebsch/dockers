9abdd5d7bcb37427fd6954be4864225293651318
Andreas Gerlach <agerlach@INNO-DEV-SFC1.innolab.local>
2016-08-11 11:07:44 +0200

    updated build script

--- nodejs-build/start_instance.sh
+++ nodejs-build/start_instance.sh
@@ -15,16 +15,16 @@ if [ -n "$GIT_REPO" ]; then
   fi
 else
   echo Copy source files to build directory...
-  cp -R /data/build/. /tmp/$BUILD_ID
+  cp -R /data/src/. /tmp/$BUILD_ID
 fi
 
 echo Cleanup obsolete files from temporary build directory...
 rm -rf /tmp/$BUILD_ID/node_modules /tmp/$BUILD_ID/.git
 
-if [ -f /tmp/$BUILD_ID/dependencies.lst ]; then
-  echo Installing native dependencies...
+if [ -f /tmp/$BUILD_ID/devDependencies.lst ]; then
+  echo Installing native dev dependencies...
   apk update
-  MODULES=$(cat /tmp/$BUILD_ID/dependencies.lst | tr '\n' ' ')
+  MODULES=$(cat /tmp/$BUILD_ID/devDependencies.lst | tr '\n' ' ')
   apk add $MODULES
   rm -rf /var/cache/apk/*
 fi
@@ -38,33 +38,16 @@ if [ -f bower.json ]; then
   bower install --allow-root
 fi
 
-if [ -f webpack.config.js ]; then
-  echo Install webpack...
-  npm i -g webpack
-fi
-
 if [ -f package.json ]; then
   echo Running npm install in temporary build directory...
   npm install --unsafe-perm && npm prune && npm cache clean
-fi
-
-if ls Gruntfile* 1> /dev/null 2>&1; then
-  echo Installing Grunt...
-  npm i -g grunt-cli
-  echo Starting Grunt build process in temporary build directory...$@
-  grunt $@
-fi
-
-if ls Gulpfile* 1> /dev/null 2>&1; then
-  echo Installing Gulp...
-  npm i -g gulp
-  echo Starting Gulp build process in temporary build directory...$@
-  gulp $@
+  echo Starting npm build process...
+  npm run build -- $BUILD_ARGS
 fi
 
 if [ -n "$PROJ_NAME" ]; then
   echo Generating Release $PROJ_NAME-$PROJ_VER-Release.tar.gz
-  tar -czf /data/build/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
+  tar -czf /data/src/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
 fi
 
 echo Finished.
