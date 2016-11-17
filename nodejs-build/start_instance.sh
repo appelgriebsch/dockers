4bb488859bcc9847dfdde692f9516bb4b56c363b
Andreas Gerlach <andi@appelgriebsch.org>
2016-11-17 19:32:28 +0100

    updated build scripts

--- nodejs-build/start_instance.sh
+++ nodejs-build/start_instance.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 
 BUILD_ID=$(uuidgen)
+SRC_DIR=/tmp/$BUILD_ID
 
-echo Creating temporary build directory /tmp/$BUILD_ID
-mkdir -p /tmp/$BUILD_ID
+echo Creating temporary build directory $SRC_DIR
+mkdir -p $SRC_DIR
 
 if [ -n "$GIT_REPO" ]; then
-  echo Cloning GIT repo from $GIT_REPO
+  echo Cloning GIT repo from $GIT_REPO to $SRC_DIR
 
   if [ -n "$GIT_USER" ]; then
-    git clone "https://$GIT_USER:$GIT_TOKEN@$GIT_REPO" /tmp/$BUILD_ID
+    git clone "https://$GIT_USER:$GIT_TOKEN@$GIT_REPO" $SRC_DIR
   else
-    git clone "https://$GIT_REPO" /tmp/$BUILD_ID
+    git clone "https://$GIT_REPO" $SRC_DIR
+  fi
+  if [ -f $SRC_DIR/.gitmodules ]; then
+    cd $SRC_DIR
+    git submodules update --init
   fi
 else
-  echo Copy source files to build directory...
-  cp -R /data/src/. /tmp/$BUILD_ID
+  echo Copy source files to build directory... $SRC_DIR
+  cp -R /data/src/. $SRC_DIR
 fi
 
 echo Cleanup obsolete files from temporary build directory...
-rm -rf /tmp/$BUILD_ID/node_modules /tmp/$BUILD_ID/.git
+rm -rf $SRC_DIR/{.git,node_modules}
 
-if [ -f /tmp/$BUILD_ID/devDependencies.lst ]; then
+echo Checking for dev dependencies....
+if [ -f $SRC_DIR/devDependencies.lst ]; then
   echo Installing native dev dependencies...
   apk update
-  MODULES=$(cat /tmp/$BUILD_ID/devDependencies.lst | tr '\n' ' ')
+  MODULES=$(cat $SRC_DIR/devDependencies.lst | tr '\n' ' ')
   apk add $MODULES
   rm -rf /var/cache/apk/*
 fi
 
-cd /tmp/$BUILD_ID
+echo Preparing build environment...
+if [ -f $SRC_DIR/prepare_env.sh ]; then
+  echo Setting up build environment...
+  cat $SRC_DIR/prepare_env.sh
+  chmod 755 $SRC_DIR/prepare_env.sh
+  $SRC_DIR/prepare_env.sh
+fi
+
+cd $SRC_DIR
 
 if [ -f bower.json ]; then
   echo Installing Bower...
@@ -47,7 +61,7 @@ fi
 
 if [ -n "$PROJ_NAME" ]; then
   echo Generating Release $PROJ_NAME-$PROJ_VER-Release.tar.gz
-  tar -czf /data/src/$PROJ_NAME-$PROJ_VER-Release.tar.gz .
+  tar -czvf /data/src/$PROJ_NAME-$PROJ_VER-Release.tar.gz -C $SRC_DIR/ .
 fi
 
 echo Finished.
