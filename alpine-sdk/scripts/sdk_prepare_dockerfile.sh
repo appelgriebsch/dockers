d84106ea4f71c169acecc9a0b663cc43960330a0
Andreas Gerlach <andi@appelgriebsch.org>
2016-12-09 15:25:43 +0100

    bugfix Dockerfile.template > Dockerfile (wrong final location)

--- alpine-sdk/scripts/sdk_prepare_dockerfile.sh
+++ alpine-sdk/scripts/sdk_prepare_dockerfile.sh
@@ -4,9 +4,9 @@ function sdk::prepareDockerfile() {
   local TM=$(date -Iminutes)
 
   if [ -f $1/Dockerfile.template ]; then
-    cat $1/Dockerfile.template > $1/Dockerfile
-    fs::replaceInFile $1/Dockerfile '$PROJ_NAME' "$PROJ_NAME"
-    fs::replaceInFile $1/Dockerfile '$PROJ_VER' "$PROJ_VER"
-    fs::replaceInFile $1/Dockerfile '$BUILD_TIME' "$TM"
+    cat $1/Dockerfile.template > /data/build/Dockerfile
+    fs::replaceInFile /data/build/Dockerfile '$PROJ_NAME' "$PROJ_NAME"
+    fs::replaceInFile /data/build/Dockerfile '$PROJ_VER' "$PROJ_VER"
+    fs::replaceInFile /data/build/Dockerfile '$BUILD_TIME' "$TM"
   fi
 }
