638f62d786e76940961d85e5154f5c250bebbbf8
Andreas Gerlach <andi@appelgriebsch.org>
2016-12-12 14:03:02 +0100

    old go-build 1.6 doesnt support glide

--- go-build/scripts/go_build_target.sh
+++ go-build/scripts/go_build_target.sh
@@ -4,11 +4,6 @@ function go::buildTarget() {
 
   cd $SRC_DIR
 
-  if [ -f $SRC_DIR/glide.yaml ]; then
-    echo Installing dependencies...
-    glide install -v
-  fi
-
   go get -d
   go build -o $PROJ_NAME $BUILD_ARGS
 }
